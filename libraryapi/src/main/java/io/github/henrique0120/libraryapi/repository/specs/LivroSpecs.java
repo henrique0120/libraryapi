2de130802070750084eca41f2a869dc2dbff070e
henrique0120 <henrique.sl959@hotmail.com>
2026-06-21 19:00:45 -0300

    Specifications

--- libraryapi/src/main/java/io/github/henrique0120/libraryapi/repository/specs/LivroSpecs.java
+++ libraryapi/src/main/java/io/github/henrique0120/libraryapi/repository/specs/LivroSpecs.java
@@ -2,6 +2,8 @@ package io.github.henrique0120.libraryapi.repository.specs;
 
 import io.github.henrique0120.libraryapi.model.GeneroLivro;
 import io.github.henrique0120.libraryapi.model.Livro;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
 
 public class LivroSpecs {
@@ -30,5 +32,12 @@ public class LivroSpecs {
                         root.get("dataPublicacao"), cb.literal("YYYY")), anoPublicacao.toString());
     }
 
+    public static Specification<Livro> nomeAutorLike(String nome){
+        return (root, query, cb) -> {
+            Join<Object, Object> joinAutor = root.join("autor", JoinType.LEFT);
+            return cb.like(cb.upper(joinAutor.get("nome")), "%" + nome.toUpperCase() + "%");
 
+//            return cb.like(cb.upper(root.get("autor").get("nome")), "%" + nome.toUpperCase() + "%");
+        };
+    }
 }
